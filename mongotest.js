e66702be3628b9b54562c937ab7a55cb6bfcf27e
Oliver <ochampet@gmail.com>
2016-10-12 01:01:47 -0400

    change port parenthesis

--- mongotest.js
+++ mongotest.js
@@ -55,5 +55,5 @@ app.use(function(err, req, res, next){
 });
 
 console.log('starting the Express (NodeJS) Web server');
-app.listen(8080;
+app.listen(8080);
 console.log('Webserver is listening on port 80');
