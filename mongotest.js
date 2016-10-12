2e3297350fe575b018e1822f894fca916345468b
Oliver <ochampet@gmail.com>
2016-10-12 00:15:57 -0400

    first commit

---
+++ mongotest.js
@@ -0,0 +1,59 @@
+var http      = require('http');
+var mongoose  = require('mongoose');
+var express   = require('express');
+
+var app    = express();
+var db;
+
+var config = {
+      "USER"    : "root",           
+      "PASS"    : "C5cEbm1kbYIH",
+      "HOST"    : "ec2-54-189-192.132.us-west-2.compute.amazonaws.com",  
+      "PORT"    : "27017", 
+      "DATABASE" : "my_example"
+    };
+	
+var dbPath  = "mongodb://"+config.USER + ":"+
+    config.PASS + "@"+
+    config.HOST + ":"+
+    config.PORT + "/"+
+    config.DATABASE;
+var standardGreeting = 'Hello World!';
+
+var greetingSchema = mongoose.Schema({
+  sentence: String
+}); 
+var Greeting= mongoose.model('Greeting', greetingSchema);
+
+db = mongoose.connect(dbPath);
+
+mongoose.connection.once('open', function() {
+  var greeting;
+  Greeting.find( function(err, greetings){
+   if( !greetings ){     
+      greeting = new Greeting({ sentence: standardGreeting }); 
+      greeting.save();
+    } 
+  }); 
+});
+
+
+app.get('/', function(req, res){
+  Greeting.findOne(function (err, greeting) {
+    res.send(greeting.sentence);
+  });
+});
+
+
+app.use(function(err, req, res, next){
+  if (req.xhr) {
+    res.send(500, 'Something went wrong!');
+  }
+  else {
+    next(err);
+  }
+});
+
+console.log('starting the Express (NodeJS) Web server');
+app.listen(80);
+console.log('Webserver is listening on port 80');
